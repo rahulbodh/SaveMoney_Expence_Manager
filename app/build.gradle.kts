fcf7c4868271566e7a35fd9ed32af7e57154328c
Rahul <rahulbodh819@gmail.com>
2025-01-22 23:55:21 +0530

    create data card on home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.circleimageview)
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
--- app/src/main/java/com/example/expencemanagerapp/view/HomeFragment.kt
+++ app/src/main/java/com/example/expencemanagerapp/view/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.expencemanagerapp.view
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,10 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.example.expencemanagerapp.R
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+
 class HomeFragment : Fragment() {
 
     private lateinit var search: ImageView
     private lateinit var userInfo :ImageView
+    private lateinit var piChart : PieChart
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +27,7 @@ class HomeFragment : Fragment() {
 
         search = view.findViewById(R.id.searchIcon)
         userInfo= view.findViewById(R.id.userInfo)
+        piChart = view.findViewById(R.id.pieChart_view);
 
         search.setOnClickListener {
             val intent = Intent(activity, SearchActivity::class.java)
@@ -31,6 +39,54 @@ class HomeFragment : Fragment() {
             startActivity(intent)
         }
 
+        showPieChart()
+
         return view
     }
+
+    private fun showPieChart() {
+        val pieEntries = ArrayList<PieEntry>()
+        val label = "type"
+
+        // Initializing data
+        val typeAmountMap = mapOf(
+            "Groceries" to 200,
+            "Rents" to 230,
+            "Clothes" to 100,
+            "Stationary" to 500,
+            "Phone" to 50
+        )
+
+        // Initializing colors for the entries
+        val colors = listOf(
+            Color.parseColor("#304567"),
+            Color.parseColor("#309967"),
+            Color.parseColor("#476567"),
+            Color.parseColor("#890567"),
+            Color.parseColor("#a35567"),
+            Color.parseColor("#ff5f67"),
+            Color.parseColor("#3ca567")
+        )
+
+        // Input data and fit data into pie chart entry
+        for ((type, amount) in typeAmountMap) {
+            pieEntries.add(PieEntry(amount.toFloat(), type))
+        }
+
+        // Collecting the entries with label name
+        val pieDataSet = PieDataSet(pieEntries, label).apply {
+            valueTextSize = 6f // Setting text size of the value
+            setColors(colors)   // Providing color list for coloring different entries
+        }
+
+        // Grouping the data set from entry to chart
+        val pieData = PieData(pieDataSet).apply {
+            setDrawValues(true) // Showing the value of the entries
+        }
+
+        // Setting the data to PieChart
+        piChart.data = pieData
+        piChart.invalidate() // Refreshing the chart
+    }
+
 }
